564380a9d42231b4e72f8db54411030cf7cd0115
shushiro <miriam.uni@outlook.de>
2022-12-04 22:47:38 +0100

    move part one of day 2 to function

--- day2/main.lua
+++ day2/main.lua
@@ -15,27 +15,31 @@ function lines_from(file)
   return lines
 end
 
-local file = 'challenge_input.txt'
-local lines = lines_from(file)
-
-local score = 0
-
-for index,game in pairs(lines) do
-  if (game == 'A X' or game == 'B Y' or game == 'C Z') then
-    score = score + 3
+function calculate_score_by_input(lines)
+  local score = 0
+  for index,game in pairs(lines) do
+    if (game == 'A X' or game == 'B Y' or game == 'C Z') then
+      score = score + 3
+    end
+    if (game == 'A Y' or game == 'B Z' or game == 'C X') then
+      score = score + 6
+    end
+    if (string.match(game, 'X')) then
+      score = score +1
+    end
+    if (string.match(game, 'Y')) then
+      score = score + 2
+    end
+    if (string.match(game, 'Z')) then
+      score = score + 3
+    end
   end
-  if (game == 'A Y' or game == 'B Z' or game == 'C X') then
-    score = score + 6
-  end
-  if (string.match(game, 'X')) then
-    score = score +1
-  end
-  if (string.match(game, 'Y')) then
-    score = score + 2
-  end
-  if (string.match(game, 'Z')) then
-    score = score + 3
-  end
+  return score
 end
 
-print('score: ', score)
+
+local file = 'challenge_input.txt'
+local lines = lines_from(file)
+score_by_input = calculate_score_by_input(lines)
+
+print('score: ', score_by_input)
