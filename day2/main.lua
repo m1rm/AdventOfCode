f24330b5db67d7449a4deda26819ff2b354977d4
shushiro <miriam.uni@outlook.de>
2022-12-04 15:51:26 +0100

    add input file validation and read file content

--- day2/main.lua
+++ day2/main.lua
@@ -1,1 +1,23 @@
-print('hello lua!')
+local io = require "io"
+
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+local file = 'exemplary_input.txt'
+local lines = lines_from(file)
+
+for k,v in pairs(lines) do
+  print(k,v)
+end
