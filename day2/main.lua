f3835f68d3a643799cce645277e307a2526ce561
shushiro <miriam.uni@outlook.de>
2022-12-05 08:23:16 +0100

    finish part 2 of day 2

--- day2/main.lua
+++ day2/main.lua
@@ -37,9 +37,54 @@ function calculate_score_by_input(lines)
   return score
 end
 
+function calculate_score_by_outcome(lines)
+  local score = 0
+  for index,game in pairs(lines) do
+    --[[ handle loss ]]--
+    if (game == 'A X' or game == 'B X' or game == 'C X') then
+      if (string.match(game, 'A')) then
+        score = score +3
+      end
+      if (string.match(game, 'B')) then
+        score = score + 1
+      end
+      if (string.match(game, 'C')) then
+        score = score + 2
+      end
+    end
+  --[[ handle draw ]]--
+    if (game == 'A Y' or game == 'B Y' or game == 'C Y') then
+      score = score + 3
+      if (string.match(game, 'A')) then
+        score = score +1
+      end
+      if (string.match(game, 'B')) then
+        score = score + 2
+      end
+      if (string.match(game, 'C')) then
+        score = score + 3
+      end
+    end
+    --[[ handle win]]--
+    if (game == 'A Z' or game == 'B Z' or game == 'C Z') then
+      score = score + 6
+      if (string.match(game, 'A')) then
+        score = score +2
+      end
+      if (string.match(game, 'B')) then
+        score = score + 3
+      end
+      if (string.match(game, 'C')) then
+        score = score + 1
+      end
+    end
+  end
+  return score
+end
+
 
 local file = 'challenge_input.txt'
 local lines = lines_from(file)
-score_by_input = calculate_score_by_input(lines)
-
-print('score: ', score_by_input)
+--[[score_by_input = calculate_score_by_input(lines)]]--
+local score_by_outcome = calculate_score_by_outcome(lines)
+print(score_by_outcome)
