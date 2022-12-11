674062ca1fb513f706bd0383bb332dca77917352
shushiro <miriam.uni@outlook.de>
2022-12-11 15:41:00 +0100

    wip find ranges containing each other by splitting strings and comparing starts and ends

--- day4/main.lua
+++ day4/main.lua
@@ -4,6 +4,24 @@ local filereader = require "filereader"
 
 local input = filereader.lines_from('challenge_input.txt')
 
-for index,sections in pairs(input) do
-  print(sections)
+function split(s, delimiter)
+     local result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
 end
+
+local score = 0
+for index,sections in pairs(input) do
+  local splitPairs = split(sections, ',')
+  local splitPair1 = split(splitPairs[1], '-')
+  local splitPair2 = split(splitPairs[2], '-')
+
+  if ((splitPair1[1] <= splitPair2[1] and splitPair1[2] <= splitPair2[2]) or (splitPair1[1] >= splitPair2[1] and splitPair1[2] >= splitPair2[2])) then
+    score = score +1
+  end
+
+end
+
+print(score)
