4a47041aec502d7e1194874714e8138fe9c3e101
shushiro <miriam.uni@outlook.de>
2022-12-11 19:34:59 +0100

    solve part 1 of day 5 & add some exemplary input

---
+++ day5/main.py
@@ -0,0 +1,34 @@
+def readInput():
+    with open('challenge_input.txt') as f:
+        return f.readlines()
+
+def main():
+    # Part One
+    input = readInput()
+    crateStacks = [
+        ['G', 'T', 'R', 'W'],
+        ['G', 'C', 'H', 'P', 'M', 'S', 'V', 'W'],
+        ['C', 'L', 'T', 'S', 'G', 'M'],
+        ['J', 'H', 'D', 'M', 'W', 'R', 'F'],
+        ['P', 'Q', 'L', 'H', 'S', 'W', 'F', 'J'],
+        ['P', 'J', 'D', 'N', 'F', 'M', 'S'],
+        ['Z', 'B', 'D', 'F', 'G', 'C', 'S', 'J'],
+        ['R', 'T', 'B'],
+        ['H', 'N', 'W', 'L', 'C']
+    ]
+
+    for instruction in input:
+        instruction = instruction.strip()
+        splitInstructions = instruction.split(' ')
+        # index 1 = num to move
+        # index 3 = pile to move from -> pop out of array
+        # index 5 = pile to move to -> push to array
+        for i in range(1, int(splitInstructions[1])+1):
+            toMove = crateStacks[int(splitInstructions[3])-1].pop()
+            crateStacks[int(splitInstructions[5])-1].append(toMove)
+
+    for crate in crateStacks:
+        print(crate[-1])
+
+if __name__ == "__main__":
+    main()
