bceb5cc8a7a8e5ee39b0d83726205d0477ccff04
shushiro <miriam.uni@outlook.de>
2022-12-13 15:33:25 +0100

    wip solve day7; files are counted only once when on dir level but should be counted multiple times

--- day7/main.py
+++ day7/main.py
@@ -2,10 +2,38 @@ def readInput(filename):
     with open(filename) as f:
         return f.readlines()
 
+def isInteger(string):
+    string = string.strip()
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
 def main():
     input = readInput('exemplary_input.txt')
-    for line in input:
-        print(line)
+    directories = {}
+    fileSizesSum = 0
+    tmpIndex = 0
+    for index,line in enumerate(input):
+        lineContent = line.split(' ')
+        if (lineContent[1].strip() == 'ls'):
+            tmpIndex = index
+            directories[tmpIndex] = []
+        if (isInteger(lineContent[0])):
+            directories[tmpIndex].append(int(lineContent[0]))
+
+    for directoryList in directories.values():
+        if(sum(directoryList) <= 100000):
+            print(sum(directoryList))
+            fileSizesSum += sum(directoryList)
+
+    print(fileSizesSum)
+
+
+
+    # Todo: if an ls occours, make directory_sum first and check that one against condition
+
 
 if __name__ == "__main__":
     main()
