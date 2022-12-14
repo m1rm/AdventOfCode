3be36749febcba20c5c61ac6b7c2d1c48157d7b0
shushiro <miriam.uni@outlook.de>
2022-12-14 08:56:36 +0100

    wip: works but too imperformant for challenge input

--- day7/main.py
+++ day7/main.py
@@ -1,3 +1,5 @@
+import copy
+
 def readInput(filename):
     with open(filename) as f:
         return f.readlines()
@@ -18,15 +20,26 @@ def isDirectory(string):
         return False
 
 # return False if any directory still contains a string == a nested dir key instead of the nested dirs values
-def isDirectoryReplacementComplete(directories):
-    if (any(isinstance(x, str) for directory in directories for x in directory)):
-        return False
-    else:
+def isDirectoryReplacementIncomplete(directories):
+    if (any(isinstance(x, str) for directory in directories.values() for x in directory)):
         return True
+    else:
+        return False
+
+# at each step in this challenge, we will have a maximum nesting of one
+def flattenList(nestedList):
+    tmp = []
+    for entry in nestedList:
+        if (type(entry) == list):
+            for item in entry:
+                tmp.append(item)
+        else:
+            tmp.append(entry)
+    return tmp
 
 
 def main():
-    input = readInput('exemplary_input.txt')
+    input = readInput('challenge_input.txt')
     directories = {}
     fileSizesSum = 0
     tmpIndex = ''
@@ -43,9 +56,30 @@ def main():
             if (isDirectory(lineContent[0])):
                 directories[tmpIndex].append(lineContent[1].strip())
 
-# {'/': [14848514, 8504156], 'a': [29116, 2557, 62596], 'e': [584], 'd': [4060174, 8033020, 5626152, 7214296]}
-    print(directories)
-    print(isDirectoryReplacementComplete(directories))
+# print(directories) # {'/': ['a', 14848514, 8504156, 'd'], 'a': ['e', 29116, 2557, 62596], 'e': [584], 'd': [4060174, 8033020, 5626152, 7214296]}
+    directoriesClone = copy.deepcopy(directories)
+
+    while (True):
+        for key, directory in directories.items():
+            for entry in directory:
+                if (entry in directories.keys()):
+                    # if current entry is a key, replace it with with original entries value at that key
+                    entryIndex = directory.index(entry)
+                    directory[entryIndex] = directoriesClone[entry]
+
+        # flatten dict values
+        for key, directory in directories.items():
+            directories[key] = flattenList(directory)
+        # check if all strings in values == nested dirs have been resolved
+        keepLooping = (isDirectoryReplacementIncomplete(directories))
+        if not keepLooping:
+            break
+
+    total = 0
+    for directory in directories.values():
+        if (sum(directory) <= 100000):
+            total += sum(directory)
+    print(total)
 
 if __name__ == "__main__":
     main()
