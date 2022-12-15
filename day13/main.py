043019e41ea655cd0b8d2655225a81eb529e5369
shushiro <miriam.uni@outlook.de>
2022-12-15 15:46:20 +0100

    process input to list of pairs

--- day13/main.py
+++ day13/main.py
@@ -1,15 +1,31 @@
 # challenge: sum of indices of pairs that are already in right order in input
 import ast
+
 def readInput(file):
     with open(file) as f:
-        return f.readlines()
+        for line in f.readlines():
+            line = line.strip()
+            if line != '':
+                yield line
 
-def main():
-    input = readInput('exemplary_input.txt')
+def convertStringInputToLiteralInput(input):
+    output = []
     for line in input:
         line = line.strip()
-        if(line != ''):
-            print(ast.literal_eval(line))
+        output.append(line)
+    return output
+
+def split(inputList, chunkSize):
+    for i in range(0, len(inputList), chunkSize):
+        yield inputList[i:i+chunkSize]
+
+def main():
+    rawInput = readInput('exemplary_input.txt')
+    input = convertStringInputToLiteralInput(rawInput)
+    inputPairs = split(input, 2)
+    for pair in inputPairs:
+        print(pair)
+
             # loop and compare i and i+1 and skip every even i (if loop starts at 1)
 
 
