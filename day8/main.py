f43821c2f0c1b32808f30feab762085431c4812e
shushiro <miriam.uni@outlook.de>
2022-12-16 13:52:15 +0100

    wip check using nested arrays

--- day8/main.py
+++ day8/main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 def readInput(filename):
     tmp = []
     with open(filename) as f:
@@ -13,22 +11,59 @@ def readInput(filename):
     return tmp
 
 def main():
-    input = readInput('exemplary_input.txt') # [[3, 0, 3, 7, 3], [2, 5, 5, 1, 2], [6, 5, 3, 3, 2], [3, 3, 5, 4, 9], [3, 5, 3, 9, 0]]
+    input = readInput('exemplary_input.txt')
+    #      0  1  2  3  4
+    #  0 [[3, 0, 3, 7, 3],
+    #  1 [2, 5, 5, 1, 2],
+    #  2 [6, 5, 3, 3, 2],
+    #  3 [3, 3, 5, 4, 9],
+    #  4 [3, 5, 3, 9, 0]]
 
-    df = pd.DataFrame(input)
-    #df["rowSum"] = df.sum(axis=1)
-    #below does not take directions into account
-    #if value > max(df.iloc[rowIndex].tolist()) and value > max(df.iloc[:, colIndex].tolist()):
+    sumTrees = 0
+    for rowIndex, row in enumerate(input):
+        print(row)
+        if (rowIndex == 0 or rowIndex+1 == len(input)):
+            continue #bc. outer rows are always visible
+        else:
+            for colIndex, value in enumerate(row):
+                if(colIndex == 0 or colIndex+1 == len(row)):
+                    continue #bc. outer cols are always visible
+                else:
+                    # make ranges for left, right, upper and lower
+                    # neighbours and check if max of value is highest
+                    # as soon as it is highest for one direction of neighbour
+                    # add one to the sum of visible trees from outside the grid
 
-    #print(df)
-    #print('###################')
-    numTrees = 0
-    for rowIndex, row in df.iterrows():
-        for colIndex, value in row.items():
-            row = df.iloc[rowIndex].tolist()
-            col = df.iloc[:, colIndex].tolist()
+                    # left is all lower col indices and equal row index
+                    # right is vice versa left
+                    # up is all lower row indices and equal col index
+                    # down is vice versa
+                    leftInRow = input[rowIndex][:colIndex]
+                    rightInRow = input[rowIndex][colIndex+1:]
+                    upperRows = input[:rowIndex-1]
+                    lowerRows = input[rowIndex+1:]
+                    print(leftInRow)
+                    print(rightInRow)
+                    print(upperRows)
+                    print(lowerRows)
+
+                    if (value > max(leftInRow) or value > max(rightInRow)):
+                        print('Item at: ', rowIndex ,'|', colIndex, 'is visible due to row check')
+                        sumTrees += 1
+                    else:
+                        tmp = []
+                        for row in upperRows:
+                            tmp.append(row[colIndex])
+                        if (value > max(tmp)):
+                            sumTress += 1
+                        else:
+                            tmp = []
+                            for row in lowerRows:
+                                tmp.append(row[colIndex])
+                                if (value > max(tmp)):
+                                    sumTress += 1
 
 
-
+    print(sumTrees)
 if __name__ == "__main__":
     main()
