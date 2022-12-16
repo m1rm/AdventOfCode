5e979e33c1c4828cf76091080d14abf30d179db0
shushiro <miriam.uni@outlook.de>
2022-12-16 09:59:10 +0100

    wip false and too complicated manual check for partial overlaps

--- day4/main.py
+++ day4/main.py
@@ -2,21 +2,50 @@ def readInput():
     with open('challenge_input.txt') as f:
         return f.readlines()
 
+def isFullyOverlapping(splitPair1, splitPair2):
+    if int(splitPair1[0]) >= int(splitPair2[0]) and int(splitPair1[1]) <= int(splitPair2[1]) or int(splitPair1[0]) <= int(splitPair2[0]) and int(splitPair1[1]) >= int(splitPair2[1]):
+        return True
+    else:
+        return False
+
+def isPartlyOverlapping(splitPair1, splitPair2):
+    start1 = int(splitPair1[0])
+    end1 = int(splitPair1[1])
+    start2 = int(splitPair2[0])
+    end2 = int(splitPair2[1])
+
+    if isFullyOverlapping(splitPair1, splitPair2):
+        return True
+    else:
+        if (start1 < start2 and end1 < end2 and start2 < end1) or (start2 < start1 and start1 < end2 and end2 < end1) or (start2 < start1 and end2 < end1 and start1 < end2) or (start1 < start2 and start2 < end1 and end1 < end2):
+            return True
+        else:
+            return False
+
 def main():
     # Part One
     input = readInput()
-    score = 0
+    fullyScore = 0
+    partlyScore = 0
     # '31-69,35-55\n'
     for pairs in input:
         pairs = pairs.strip()
         splitPairs = pairs.split(',')
         splitPair1 = splitPairs[0].split('-')
         splitPair2 = splitPairs[1].split('-')
-        if ((splitPair1[0] >= splitPair2[0] and splitPair1[1] <= splitPair2[1]) or (splitPair1[0] <= splitPair2[0] and splitPair1[1] >= splitPair2[1])):
-            score = score +1
+
+        fullyOverlapping = isFullyOverlapping(splitPair1, splitPair2)
+        partlyOverlapping = isPartlyOverlapping(splitPair1, splitPair2)
+
+        if fullyOverlapping:
+            fullyScore += 1
+
+        if partlyOverlapping:
+            partlyScore += 1
 
 
-    print(score)
+    print('Pairs fully overlapping: ', fullyScore)
+    print('Pairs partly overlapping: ', partlyScore)
 
 if __name__ == "__main__":
     main()
