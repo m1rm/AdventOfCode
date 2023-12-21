e1c00c14e92dcbefaed47dc790d65af752ffff04
Miriam Mueller <m.mueller@meta-fusion.com>
2023-12-21 12:46:45 +0100

    solve day 8 part 1

--- 2023/day8/main.js
+++ 2023/day8/main.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 function parseNodes(nodeDefinitions) {
-    let nodes = []
+    let nodes = new Map()
     for (nodeDefinition of nodeDefinitions) {
         const splitDefinition = nodeDefinition.split('=').map(function(item) {
             return item.trim();
@@ -9,21 +9,51 @@ function parseNodes(nodeDefinitions) {
         const adjacentNodes = splitDefinition[1].split(',').map(function(item) {
             return item.trim().replace('(', '').replace(')', '');
         });
-        nodes[splitDefinition[0]] = {'L': adjacentNodes[0], 'R': adjacentNodes[1]}
+        nodes.set(splitDefinition[0], {'L': adjacentNodes[0], 'R': adjacentNodes[1]})
     }
-    console.log(nodes)
+    // console.log(nodes)
+    return nodes
+}
+
+function walkPath(nodes, instructions) {
+    // console.log(instructions)
+    // console.log(nodes)
+    let nextNode = 'AAA'
+    let steps = 0
+    for (let i = 0; i <= instructions.length; i++) {
+        if (nextNode === 'ZZZ') {
+            return steps
+        }         
+        // reset i to keep looping over instructions if 
+        // 'ZZZ' was not yet reached
+        if (i === instructions.length) {
+            i = 0
+        } 
+
+        nodeVal = nodes.get(nextNode)
+        tmp = nodeVal[instructions[i]]
+        steps +=1
+        nextNode = tmp    
+    }
 }
 
 function main() {
-    const input = fs.readFileSync('test_input.txt', 'utf8').trimEnd();
+    const input = fs.readFileSync('challenge_input.txt', 'utf8').trimEnd();
 
     const lines = input.split("\n").filter(element => element);
-    const instructions = lines[0]
+
+    const instructionString = lines[0]
+    const instructions = instructionString.split('')
     const nodeDefinitions = lines.splice(1,)
-    parseNodes(nodeDefinitions)
 
+    const nodes = parseNodes(nodeDefinitions)
+    // console.log(nodes)
 
-    // console.log(instructions, nodeDefinitions)
+    const steps = walkPath(nodes, instructions)
+    console.log(steps)
+
+    
+    
 }
 
 
