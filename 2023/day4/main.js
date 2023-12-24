09dadbc3952221cf40ea1854ecfd6cad8e93f6ec
Miriam Mueller <m.mueller@meta-fusion.com>
2023-12-24 13:00:34 +0100

    wip solve part2

--- 2023/day4/main.js
+++ 2023/day4/main.js
@@ -27,6 +27,20 @@ function findIntersection(game) {
     return filteredArray
 }
 
+function findWonCopies(n, game) {
+    console.log('Game ', n, ': ', game)
+    let wonCopies = []
+    const filteredArray = game[0].filter(value => game[1].includes(value));
+    console.log('filteredArray: ', filteredArray)
+    if (filteredArray.length > 0) {
+        for (let i = 0; i < filteredArray.length; i++) {
+            wonCopy = n+i+1
+            wonCopies.push(wonCopy)
+        }
+    }
+    return wonCopies
+}
+
 function calculatePoints(result) {
     if (result.length === 0) {
         return 0
@@ -44,15 +58,31 @@ function countPoints(winningNumbers) {
       return sum
 }
 
+function evaluateScratchcards(matrix) {
+
+    let wonCopiesPerGame = []
+    // TODO: loop this & abort if no more copies are yielded
+    for(let i = 0; i < matrix.length; i++) {
+        const game = matrix[i]
+        const wonCopies = findWonCopies(i+1, game)
+        wonCopiesPerGame.push(wonCopies)
+    }
+    return wonCopiesPerGame
+}
+
 function main() {
-    const input = fs.readFileSync('challenge_input.txt', 'utf8').trimEnd();
+    const input = fs.readFileSync('test_input.txt', 'utf8').trimEnd();
     
     const matrix = parseInput(input)
 
     const winningNumbersPerGame = evaluateGames(matrix)
-
     const points = countPoints(winningNumbersPerGame)
     console.log('Part 1: ', points)
+
+    const processScratchcards = evaluateScratchcards(matrix)
+    console.log(processScratchcards)
+    // const totalCards = countScratchcards(processScratchcards)
+    // console.log('Part 2: ', totalCards)
     
 }
 
